732952d4fbdc64408176ca04198c7e6fc1c5741e
Talanda Williams <talandaw@gmail.com>
2018-03-10 22:29:10 -0800

    updated to flask
    changes from default python to using flask and added javascript to  manipulate webpage.

--- hangman.py
+++ hangman.py
@@ -3,20 +3,23 @@
 # Python Hangman
 
 import random #used for picking the word from the list
+import json
+from flask import Flask, render_template, request, jsonify
+app = Flask(__name__)
 
 class Hangman():
     def __init__(self, guessword):  #constructor of Hangman class
         self.guessword = guessword
-        self.numberGuesses = 0
         self.displayword = len(guessword) * '_' #used to show guess progress
         self.wrong_guesses = 0 
         self.guessed = [] #used for containing prior guesses
 
     def __str__(self): #used for printing user progress/display world with a space for readability
-        word =''
+        word = ''
         for char in self.displayword:
             word += f" {char}"
         return word
+        #return self.displayword
 
     def guess(self, char):
         char = char.casefold() # casefold makes guesses case insensitive
@@ -50,29 +53,66 @@ def pickrandomword(): #function that pulls words from list and picks one randoml
         linerange = len(all_words)
         return all_words[random.randrange(0, linerange)]
 
+@app.route('/')
+def initialize():
+    #if request.form.get("displayword") is None:
+    word = pickrandomword()  # step 1, pick word behind scenes
+    game = Hangman(word)  # step 2, apply it to 'game'
+    print("initialized")
+    return render_template("index.html", guessword=game.guessword, displayword=str(game),
+                           wrong_guesses=game.wrong_guesses,
+                           guessed=json.dumps(game.guessed), message="Welcome to the game")
+    #word = pickrandomword()  # step 1, pick word behind scenes
+    #game = Hangman(word)  # step 2, apply it to game
+    #print(word)
+    #return jsonify(guessword=game.guessword, displayword=game.displayword,
+                    #  numberGuesses=game.numberGuesses, wrong_guesses=game.wrong_guesses,
+                    #  guessed=game.guessed, incorrect=False, win=False, lost=False)
+
+@app.route("/", methods=["POST"])
 def main():
-    word = pickrandomword() #step 1, pick word behind scenes
-    game = Hangman(word) #step 2, apply it to game
-    while(not game.over()):
-        print(game)
-        valid_guess = False
-        while not valid_guess:
-            letter = input("Make a guess\n") #step 3, make a valid guess
-            if len(letter) != 1:
-                print("enter only one letter")
-            elif not letter.isalpha():
-                print("guess must be a letter of the alphabet")
-            elif letter in game.guessed:
-                print("you've already guessed that letter")
-            else:
-                valid_guess = True #step 4, proceeds with correct letter input
-        if not game.guess(letter):
-            print(f"wrong guess, you have {7-game.wrong_guesses} left") #step 5, display number of guesses remaining if wrong guess
-    print(game) #step 5, calls str and checks word, etc
+    render_template("index.html")
+    word = pickrandomword()  # step 1, pick word behind scenes
+    game = Hangman(word)  # step 2, apply it to game
+    # print(word)
+    game = Hangman(request.form.get("guessword"))
+    print(request.form.get("displayword"))
+    game.displayword = request.form.get("displayword").replace(' ', '')
+    game.wrong_guesses = int(request.form.get("wrong_guesses"))
+    game.guessed = json.loads(request.form.get("guessed"))
+    letter = request.form.get('guess')
+    # print (letter)
+    if not game.over():
+        # print(game)
+        if len(letter) != 1:
+            return render_template("index.html", guessword=game.guessword, displayword=str(game),
+                                    wrong_guesses=game.wrong_guesses,
+                                    guessed=json.dumps(game.guessed), incorrect=True, win=False, lost=False, message="Must be only one letter")
+        elif not letter.isalpha():
+            return render_template("index.html", guessword=game.guessword, displayword=str(game),
+                                    wrong_guesses=game.wrong_guesses,
+                                    guessed=json.dumps(game.guessed), incorrect=True, win=False, lost=False, message="Must be a letter")
+        elif letter in game.guessed:
+            return render_template("index.html", guessword=game.guessword, displayword=str(game),
+                                    wrong_guesses=game.wrong_guesses,
+                                    guessed=json.dumps(game.guessed), incorrect=True, win=False, lost=False, message="Already guessed")
+        if not game.guess(letter) and not game.over():
+            return render_template("index.html", guessword=game.guessword, displayword=str(game),
+                                   wrong_guesses = game.wrong_guesses,
+                                   guessed = json.dumps(game.guessed), incorrect=False, win=False, lost=False, message="incorrect guess")
+        elif not game.over():
+            return render_template("index.html", guessword=game.guessword, displayword=str(game),
+                                   wrong_guesses=game.wrong_guesses,
+                                   guessed=json.dumps(game.guessed), incorrect=False, win=False, lost=False, message="correct guess")
+    # print(game) #step 5, calls str and checks word, etc
     if not game.win():
-        print("The word was", game.guessword) #step 6, lose game
+        return render_template("index.html", guessword=game.guessword, displayword=str(game),
+                               wrong_guesses=game.wrong_guesses,
+                               guessed=json.dumps(game.guessed), incorrect=True, win=False, lost=True, message="You Lose")
     else:
-        print("you win") #or better step 6, win game.
+        return render_template("index.html", guessword=game.guessword, displayword=str(game),
+                               wrong_guesses=game.wrong_guesses,
+                               guessed=json.dumps(game.guessed), incorrect=True, win=True, lost=False, message="you win")
 
 if __name__ == "__main__": #runs actual game code above
     main()
